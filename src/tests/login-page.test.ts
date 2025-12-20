aa85b01402fdc34071b17d5fe7aa2f189d7524d0
Hummam <hummamhabib@gmail.com>
2025-12-20 15:36:08 +0000

    updated code

--- src/tests/login-page.test.ts
+++ src/tests/login-page.test.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
+import { decrypt } from '../utils/CryptojsUtil';
 
 test("Login Page - Valid Credentials", async ({ page }) => {
     // Arrange
     const loginPage = new LoginPage(page);
     await loginPage.navigateToLoginPage();
 
-    await loginPage.fillUsername("hummamhabib484@agentforce.com");
-    await loginPage.fillPassword("munited997");
+    await loginPage.fillUsername(decrypt(process.env.userid!));
+    await loginPage.fillPassword(decrypt(process.env.password!));
     const homePage = await loginPage.clickLoginButton();
     
     await page.waitForTimeout(20000); // 3 seconds
